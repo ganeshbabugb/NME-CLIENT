ca21ff62ccd58267ece04fecae4f4ebb9a88a43b
Ganesh Babu <ganeshbabugb2525@gamil.com>
2023-04-14 14:36:44 +0530

    Final commit

---
+++ src/Constants/registerFields.js
@@ -0,0 +1,62 @@
+const aidedDept = [
+  { label: "B.A.History", value: "B.A.History" },
+  { label: "B.A.English Literature", value: "B.A.English Literature" },
+  { label: "B.Sc.Mathematics", value: "B.Sc.Mathematics" },
+  { label: "B.Sc.Physics", value: "B.Sc.Physics" },
+  { label: "B.Sc.Chemistry", value: "B.Sc.Chemistry" },
+  { label: "B.Sc.Botany", value: "B.Sc.Botany" },
+  { label: "B.Sc.Zoology", value: "B.Sc.Zoology" },
+  { label: "B.Sc.Computer Science", value: "B.Sc.Computer Science" },
+  { label: "B.Sc.Nutrition & Dietetics", value: "B.Sc.Nutrition & Dietetics" },
+  { label: "B.Com.Commerce", value: "B.Com.Commerce" },
+];
+
+const unaidedDept = [
+  { label: "B.A.Tamil Literature", value: "B.A.Tamil Literature" },
+  { label: "B.A.English Literature", value: "B.A.English Literature" },
+  { label: "B.Sc.Mathematics", value: "B.Sc.Mathematics" },
+  { label: "B.Sc.Physics", value: "B.Sc.Physics" },
+  { label: "B.Sc.Biochemistry", value: "B.Sc.Biochemistry" },
+  { label: "B.C.A", value: "B.C.A" },
+  { label: "B.Sc.Computer Science", value: "B.Sc.Computer Science" },
+  {
+    label: "B.Sc.Information Technology",
+    value: "B.Sc.Information Technology",
+  },
+  { label: "B.Sc Computer Technology", value: "B.Sc Computer Technology" },
+  {
+    label: "B.Sc.Computer Science with Data Analytics",
+    value: "B.Sc.Computer Science with Data Analytics",
+  },
+  {
+    label: "B.Sc.Costume Design and Fashion",
+    value: "B.Sc.Costume Design and Fashion",
+  },
+  { label: "B.Com", value: "B.Com" },
+  {
+    label: "B.Com.Corporate Secretaryship",
+    value: "B.Com.Corporate Secretaryship",
+  },
+  { label: "B.Com.Cooperation", value: "B.Com.Cooperation" },
+  {
+    label: "B.Com.Commerce with Computer Applications",
+    value: "B.Com.Commerce with Computer Applications",
+  },
+  { label: "B.Com.E-Commerce", value: "B.Com.E-Commerce" },
+  { label: "B.B.A.(CA)", value: "B.B.A.(CA)" },
+  {
+    label: "B.Com.Professional Accounting",
+    value: "B.Com.Professional Accounting",
+  },
+  {
+    label: "B.Com.with Accounting & Finance",
+    value: "B.Com.with Accounting & Finance",
+  },
+  {
+    label: "B.Com.(Banking & Insurance)",
+    value: "B.Com.(Banking & Insurance)",
+  },
+  { label: "B.Sc.Geography", value: "B.Sc.Geography" },
+];
+
+export { aidedDept, unaidedDept };
